6905751cbee4a6d96e5ff9a8315fddfbf7e2e28d
phuong <duongtuanphuong2@gmail.com>
2023-06-09 16:54:07 +0700

    commit

--- src/app/component/client/index/index.component.ts
+++ src/app/component/client/index/index.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/_service/cart.service';
+import { ProductService } from 'src/app/_service/product.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+
+  listProduct : any;
+  listItemInCart : any;
+
+  constructor(private productService: ProductService,public cartService:CartService){}
+
+  ngOnInit(): void {
+    this.getListProduct();
+   
+  }
+
+  getListProduct(){
+    this.productService.getListProduct().subscribe(data =>{
+      this.listProduct = data;
+      this.cartService.loadCart();
+    })
+  }
+
+  addToCart(item: any){
+    this.cartService.getItems();
+    this.cartService.addToCart(item,1);
+  }
+
+  removeFromCart(item:any){
+    this.cartService.remove(item);
+  }
+
+  updateQuantity(item: any,event: any){
+    let quantity : number = event.target.value;
+    this.cartService.updateCart(item,quantity);
+  }
+
 
 }
